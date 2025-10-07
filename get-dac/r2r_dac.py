a967e7a5887cc3bc9232411cf352ea313270109b
SDarsKZN <arslanovsamat.717@gmail.com>
2025-10-07 20:05:02 +0300

    eto pipec

--- get-dac/r2r_dac.py
+++ get-dac/r2r_dac.py
@@ -5,7 +5,6 @@ class R2R_DAC:
         self.gpio_bits = gpio_bits
         self.dynamic_range = dynamic_range
         self.verbose = verbose
-
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
 
