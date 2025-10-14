2c515e875137f0bec6e8a34fbe6c6ce96482ece8
SDarsKZN <arslanovsamat.717@gmail.com>
2025-10-14 19:38:48 +0300

    Часть номера 7

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,11 @@
+import mcp4725_driver
+import signal_generator
+import time
+
+SIGNAL_FREQUENCY = 1.0     
+AMPLITUDE = 1.5           
+SAMPLING_FREQUENCY = 100    
+PWM_FREQUENCY = 500        
+DYNAMIC_RANGE = 3.29 
+
+
